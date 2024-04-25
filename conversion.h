7c9280c3ed03321e444092fe51c5aea4b14a5fa0
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2024-04-25 10:36:13 -0400

    var name chanages and comments

--- conversion.h
+++ conversion.h
@@ -16,10 +16,9 @@ bigint binToInt(const std::bitset<bit_size>& bin) {
 
     for (int i = 0; i < bit_size; i++) {
         if (bin[i]) {
-            result += big_pow(2, i); // Assuming big_pow is defined somewhere
+            result += big_pow(2, i);
         }
     }
-
     return result;
 }
 
@@ -40,7 +39,7 @@ std::string binToHex(const std::bitset<bit_size>& bin) {
 
     //hex chunks of 4 bits
     for (int i = 0; i < bin_str.size(); i += 4) {
-        // Extract a chunk of 4 bits from the binary number
+        //extract a chunk of 4 bits from the binary number
         std::bitset<4> chunk(bin_str.substr(i, 4));
 
         //chunk to decimal
@@ -102,73 +101,48 @@ std::bitset<bit_size> hexToBin(const std::string& hex) {
 
 
 /*
-    Text to Integer
-        -Input: Text as string
-        -Ouput: Integer as string
+    Hexadcimal to Text
+        -Input: hexadecimal string
+        -Output: ascii rep of each b-bit hexadecimal
+
 */
-inline bigint textToInt(const std::string& text) {
-    std::string result;
-
-    for (char c : text) {
-        std::string ascii = std::to_string(static_cast<int>(c-32));
-        // std::cout << ascii << " ";
-        if (ascii.size() == 1)
-            result += ("0"+ascii);
-        else
-            result += (ascii);
-    }
-    return to_bigint(result);
-}
-
-
-/*
-    Integer to Text
-        -Input: Integer as string
-        -Output: String as Integer
-*/
-inline std::string intToText(const bigint& integer) {
-
-    std::string int_str = integer.as_str();
+inline std::string hexToText(std::string hex) {
+    //pad hex
+    while (hex.size() % 2 != 0) hex = "0" + hex;
 
     std::string result = "";
-
-    for(int i = 0; i < int_str.size(); i+=2) {
-        unsigned int ascii = stoi(int_str.substr(i, 2)) + 32;
-        result += static_cast<char>(ascii);
-        // std::cout << ascii << ": " << static_cast<char>(ascii) << std::endl;
-    }
-    return result;
-}
-
-inline std::string hexToText(std::string text) {
-    //pad text
-    while (text.size() % 2 != 0) text = "0" + text;
-
-    std::string result = "";
-    for (int i = 0; i < text.size(); i+=2) {
-        std::string hex = text.substr(i, 2);
+    for (int i = 0; i < hex.size(); i+=2) {
+        std::string hex_i = hex.substr(i, 2);
 
         //turn to int
-        std::bitset<8> hex_bin(hexToBin<8>(hex));
-        bigint hex_int = binToInt<8>(hex_bin); //might have to add 32
-
+        std::bitset<8> hex_i_bin(hexToBin<8>(hex_i));
+        bigint hex_i_int = binToInt<8>(hex_i_bin); //might have to add 32
+        // std::cout << hex_int << std::endl;
         //int to ascii
-        result += static_cast<char>(stoi(hex_int.as_str()));
+        result += static_cast<char>(stoi(hex_i_int.as_str()));
     }
 
     //turn every two to decimal and get ascii rep
     return result;
 }
 
-inline std::string textToHex(std::string hex) {
+
+/*
+    Text to Hexadecimal
+        -Input: Text string
+        -Output: haxadeicmal rep value of char's ascii s
+*/
+inline std::string textToHex(std::string text) {
     std::string result = "";;
 
-    for (int i = 0; i < hex.size(); i++) {
+    for (int i = 0; i < text.size(); i++) {
         //hex to int
-        int ascii = static_cast<int>(static_cast<unsigned char>(hex[i]));
+        int ascii = static_cast<int>(static_cast<unsigned char>(text[i]));
 
         //int to hex
         std::string ascii_hex = binToHex<8>(intToBin<8>(ascii));
+        // std::cout << ascii_hex << std::endl;
+        // std::cout << static_cast<char>(ascii) << std::endl;
         result += ascii_hex;
     }
     return result;
